e52465d9faeb3859fab9a15b1362b57b5635afa2
yin_kio <jamycake@hotmail.com>
2022-12-26 16:34:23 +0300

    file_manager.data: add PermissionCheckerImpl.kt

---
+++ filemanager/data/src/main/java/yin_kio/file_manager/data/PermissionCheckerImpl.kt
@@ -0,0 +1,14 @@
+package yin_kio.file_manager.data
+
+import android.content.Context
+import com.example.permissions.hasStoragePermissions
+import yin_kio.file_manager.domain.gateways.PermissionChecker
+
+class PermissionCheckerImpl(
+    private val context: Context
+) : PermissionChecker {
+
+    override val hasPermission: Boolean
+        get() = context.hasStoragePermissions()
+
+}

9010f6f9696ad7db97485b0a86736db9ce908d5e
yin_kio <jamycake@hotmail.com>
2022-12-27 11:55:16 +0300

    file_manager.test: run module

--- filemanager/test/src/main/java/com/example/test/MainActivity.kt
+++ filemanager/test/src/main/java/com/example/test/MainActivity.kt
@@ -1,73 +1,24 @@
 package com.example.test
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import kotlinx.coroutines.*
+import androidx.appcompat.app.AppCompatActivity
+import yin_kio.file_manager.di.FileManagerFragmentFactory
+import yin_kio.file_manager.presentation.FileManagerFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportFragmentManager.fragmentFactory = FileManagerFragmentFactory()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val fragment = supportFragmentManager.fragmentFactory
+            .instantiate(classLoader, FileManagerFragment::class.java.name)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            withContext(Dispatchers.Main){
-                recycler.layoutManager = LinearLayoutManager(this@MainActivity)
-            }
-            delay(2000)
-            withContext(Dispatchers.Main){
-                recycler.layoutManager = GridLayoutManager(this@MainActivity, 2)
-            }
-        }
-        val adapter = Adapter()
-        recycler.adapter = adapter
-        adapter.submitList(listOf("231231", "", "", "","","","","","","","","","","","","",))
-        Log.d("!!!", "onCreate")
-
-
-
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
 
     }
 
-    private class Adapter : ListAdapter<String, ViewHolder>(
-        object : DiffUtil.ItemCallback<String>(){
-            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-                return oldItem == newItem
-            }
 
-            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-                return oldItem == newItem
-            }
-        }
-    ){
-        @SuppressLint("NewApi")
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            Log.d("!!!", "onCreateViewHolder")
-            return object : ViewHolder(TextView(parent.context).also {
-                it.text = "test"
-                it.setTextColor(parent.context.getColor(R.color.purple_200))
-            }){
-
-            }
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            Log.d("!!!", "onBindViewHolder")
-            (holder.itemView as TextView).apply {
-                text = "some text"
-            }
-        }
-    }
 }

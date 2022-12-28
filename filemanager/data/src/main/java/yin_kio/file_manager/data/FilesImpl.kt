1bc4b48347754685e20346bb29371534569d0663
yin_kio <jamycake@hotmail.com>
2022-12-28 16:09:24 +0300

    file_manager.data: fix file group getting

--- filemanager/data/src/main/java/yin_kio/file_manager/data/FilesImpl.kt
+++ filemanager/data/src/main/java/yin_kio/file_manager/data/FilesImpl.kt
@@ -19,10 +19,10 @@ internal class FilesImpl(
 
         return when(fileRequest){
             FileRequest.AllFiles -> allFiles
-            FileRequest.Images -> allFiles.filter { groups.images[it.extension.uppercase()] != null }
-            FileRequest.Video -> allFiles.filter { groups.video[it.extension.uppercase()] != null }
-            FileRequest.Documents -> allFiles.filter { groups.documents[it.extension.uppercase()] != null }
-            FileRequest.Audio -> allFiles.filter { groups.audio[it.extension.uppercase()] != null }
+            FileRequest.Images -> allFiles.filter { isImage(it.extension) }
+            FileRequest.Video -> allFiles.filter { isVideo(it.extension) }
+            FileRequest.Documents -> allFiles.filter { isDocument(it.extension) }
+            FileRequest.Audio -> allFiles.filter { isAudio(it.extension) }
         }.toFileInfoList()
     }
 
@@ -30,12 +30,31 @@ internal class FilesImpl(
         map { fileInfo(it) }
 
     private fun fileInfo(it: File) = FileInfo(
-        fileGroup = FileGroup.Images,
+        fileGroup = fileGroup(it.extension),
         name = it.name,
         time = it.lastModified(),
         size = it.length(),
         path = it.absolutePath,
     )
+
+    private fun fileGroup(extension: String?) : FileGroup{
+        return when{
+            extension == null -> FileGroup.Unknown
+            isImage(extension) -> FileGroup.Images
+            isVideo(extension) -> FileGroup.Video
+            isDocument(extension) -> FileGroup.Documents
+            isAudio(extension) -> FileGroup.Audio
+            else -> FileGroup.Unknown
+        }
+    }
+
+    private fun isAudio(extension: String) = groups.audio[extension.uppercase()] != null
+
+    private fun isDocument(extension: String) = groups.documents[extension.uppercase()] != null
+
+    private fun isVideo(extension: String) = groups.video[extension.uppercase()] != null
+
+    private fun isImage(extension: String) = groups.images[extension.uppercase()] != null
 
 
     override suspend fun delete(paths: List<String>) {

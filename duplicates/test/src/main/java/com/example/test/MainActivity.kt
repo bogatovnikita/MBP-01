b3ad128efeb3abf2cd5a2e234e1fd7a0843e06bb
yin_kio <jamycake@hotmail.com>
2023-01-14 12:30:48 +0300

    duplicates.test: add update fragment instantiation

--- duplicates/test/src/main/java/com/example/test/MainActivity.kt
+++ duplicates/test/src/main/java/com/example/test/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import yin_kio.duplicates.presentation.ParentFragment
+import yin_kio_duplicates.di.DuplicatesFragmentFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportFragmentManager.fragmentFactory = DuplicatesFragmentFactory()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, ParentFragment())
+            .replace(R.id.fragment_container, supportFragmentManager.fragmentFactory.instantiate(classLoader, ParentFragment::class.java.name))
             .commit()
     }
 }

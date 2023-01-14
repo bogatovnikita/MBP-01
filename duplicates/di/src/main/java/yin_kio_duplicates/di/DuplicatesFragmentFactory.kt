3df6363b4d12365841473ebe789cd6caaaadb172
yin_kio <jamycake@hotmail.com>
2023-01-14 12:27:49 +0300

    duplicates.di: add DuplicatesFragmentFactory.kt

---
+++ duplicates/di/src/main/java/yin_kio_duplicates/di/DuplicatesFragmentFactory.kt
@@ -0,0 +1,43 @@
+package yin_kio_duplicates.di
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import yin_kio.duplicates.domain.DuplicatesDomainFactory
+import yin_kio.duplicates.presentation.DuplicatesViewModel
+import yin_kio.duplicates.presentation.ParentFragment
+import yin_kio.file_utils.FileUtilsImpl
+import yin_kio_duplicates.data.AndroidImagesComparator
+import yin_kio_duplicates.data.FilesImpl
+import yin_kio_duplicates.data.PermissionsImpl
+
+class DuplicatesFragmentFactory : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        when{
+            className == ParentFragment::class.java.name -> {
+
+                return ParentFragment(
+                    viewModelCreator = {
+                        val (useCase, stateHolder) =  DuplicatesDomainFactory.createUseCaseAndStateHolder(
+                            coroutineScope = viewModelScope,
+                            files = FilesImpl(FileUtilsImpl()),
+                            imagesComparator = AndroidImagesComparator(),
+                            permissions = PermissionsImpl(it)
+                        )
+
+                        DuplicatesViewModel(
+                            useCase = useCase,
+                            state = stateHolder,
+                            coroutineScope = viewModelScope,
+                            coroutineDispatcher = Dispatchers.Default
+                        )
+                    }
+                )
+            }
+        }
+
+        return super.instantiate(classLoader, className)
+    }
+}

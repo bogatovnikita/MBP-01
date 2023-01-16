1366fbdeb8dab632c266dfc791ffdea63017cdfc
dimap <dima.pasyutin2@gmail.com>
2023-01-16 05:55:31 +0300

    add navigation

---
+++ notification/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/notification_manager/NotificationSettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.entertainment.event.ssearch.presentation.ui.notification_manager
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.entertainment.event.ssearch.presentation.R
+import com.entertainment.event.ssearch.presentation.databinding.FragmentNotificationSettingsBinding
+
+
+class NotificationSettingsFragment : Fragment(R.layout.fragment_notification_settings) {
+
+    private val binding: FragmentNotificationSettingsBinding by viewBinding()
+
+}

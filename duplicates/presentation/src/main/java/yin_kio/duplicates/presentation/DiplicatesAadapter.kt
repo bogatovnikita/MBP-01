92b84267cbd76f7715214e868c57aeebda103e5b
yin_kio <jamycake@hotmail.com>
2023-01-17 14:37:21 +0300

    duplicates.presentation: images showing

---
+++ duplicates/presentation/src/main/java/yin_kio/duplicates/presentation/DiplicatesAadapter.kt
@@ -0,0 +1,75 @@
+package yin_kio.duplicates.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.recycler_adapter.recyclerAdapter
+import yin_kio.duplicates.domain.models.ImageInfo
+import yin_kio.duplicates.presentation.databinding.ListItemGroupBinding
+import yin_kio.duplicates.presentation.databinding.ListItemImageBinding
+
+class DiplicatesAadapter : ListAdapter<List<ImageInfo>, DuplicatesViewHolder>(difCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DuplicatesViewHolder {
+        return DuplicatesViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: DuplicatesViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+
+
+private fun difCallback() = object : DiffUtil.ItemCallback<List<ImageInfo>>(){
+        override fun areItemsTheSame(oldItem: List<ImageInfo>, newItem: List<ImageInfo>): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: List<ImageInfo>,
+            newItem: List<ImageInfo>
+        ): Boolean {
+            return oldItem == newItem
+        }
+}
+
+
+
+
+class DuplicatesViewHolder private constructor (
+    private val binding: ListItemGroupBinding
+) : ViewHolder(binding.root){
+
+
+    private val adapter by lazy {
+        recyclerAdapter<ImageInfo, ListItemImageBinding>(
+            onBind = {item, holder ->
+
+                Glide.with(binding.root.context)
+                    .load(item.path)
+                    .into(image)
+
+            }
+        )
+    }
+
+    init {
+        binding.recycler.adapter = adapter
+    }
+
+    fun bind(item: List<ImageInfo>){
+        adapter.submitList(item)
+    }
+
+    companion object{
+        fun from(parent: ViewGroup) : DuplicatesViewHolder{
+            val binding = ListItemGroupBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return DuplicatesViewHolder(binding)
+        }
+    }
+
+}
--- duplicates/presentation/src/main/java/yin_kio/duplicates/presentation/DuplicatesFragment.kt
+++ duplicates/presentation/src/main/java/yin_kio/duplicates/presentation/DuplicatesFragment.kt
@@ -6,23 +6,14 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.example.recycler_adapter.recyclerAdapter
-import yin_kio.duplicates.domain.models.ImageInfo
 import yin_kio.duplicates.presentation.databinding.FragmentDuplicatesBinding
-import yin_kio.duplicates.presentation.databinding.ListItemGroupBinding
 
 class DuplicatesFragment : Fragment(R.layout.fragment_duplicates) {
 
     private val binding: FragmentDuplicatesBinding by viewBinding()
     private val viewModel by lazy { parentViewModel() }
 
-    private val adapter by lazy {
-        recyclerAdapter<List<ImageInfo>, ListItemGroupBinding>(
-            onBind = { item, holder ->
-                checkbox.isChecked = viewModel.isGroupSelected(holder.adapterPosition)
-            }
-        )
-    }
+    private val adapter by lazy { DiplicatesAadapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.recycler.adapter = adapter

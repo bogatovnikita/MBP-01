fba2f3cb4fe7fec2e9c5b54e8de44bb625300e27
Nikita Bogatov <bogatovnikita@gmail.com>
2023-01-19 20:00:47 +0400

    add layout for the second screen time

---
+++ screenTime/presentation/src/main/java/com/hedgehog/presentation/ui/second_screen/SecondScreenTimeFragment.kt
@@ -0,0 +1,8 @@
+package com.hedgehog.presentation.ui.second_screen
+
+import com.hedgehog.presentation.base.BaseFragment
+import com.hedgehog.presentation.databinding.FragmentSecondScreenTimeBinding
+
+class SecondScreenTimeFragment :
+    BaseFragment<FragmentSecondScreenTimeBinding>(FragmentSecondScreenTimeBinding::inflate) {
+}

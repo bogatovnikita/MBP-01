cda8529492d06d363d9226c06e66c9c625a51a09
yin_kio <jamycake@hotmail.com>
2023-01-23 16:09:08 +0300

    garbage_clean.domain: add method addTo to GarbageFiles.kt

--- garbage_clean/domain/src/test/kotlin/GarbageFilesTest.kt
+++ garbage_clean/domain/src/test/kotlin/GarbageFilesTest.kt
@@ -1,17 +1,31 @@
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import yin_kio.garbage_clean.domain.entities.GarbageFiles
 import yin_kio.garbage_clean.domain.entities.GarbageType
 
 class GarbageFilesTest {
 
+    private lateinit var garbageFiles: GarbageFiles
+
+    @BeforeEach
+    fun setup(){
+        garbageFiles = GarbageFiles()
+    }
+
     @Test
     fun testPut(){
-        val garbageFiles = GarbageFiles()
-
-        garbageFiles[GarbageType.Apk] = listOf()
+        garbageFiles[GarbageType.Apk] = mutableListOf()
 
         assertTrue(garbageFiles[GarbageType.Apk] != null)
+    }
+
+    @Test
+    fun testAddTo(){
+        val path = "/path/to/file.apk"
+        garbageFiles.addTo(GarbageType.Apk, path)
+
+        assertTrue(garbageFiles[GarbageType.Apk]!!.contains(path))
     }
 
 }

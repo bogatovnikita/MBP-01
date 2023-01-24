3014e98ca9e73b3205c0e32171f3a3f9ca6382e6
yin_kio <jamycake@hotmail.com>
2023-01-24 10:14:26 +0300

    garbage_clean.domain: add selectAll and switchSelection use cases

--- garbage_clean/domain/src/main/java/yin_kio/garbage_clean/domain/GarbageCleanerUseCases.kt
+++ garbage_clean/domain/src/main/java/yin_kio/garbage_clean/domain/GarbageCleanerUseCases.kt
@@ -1,4 +1,20 @@
 package yin_kio.garbage_clean.domain
 
-class GarbageCleanerUseCases {
+import yin_kio.garbage_clean.domain.entities.DeleteForm
+import yin_kio.garbage_clean.domain.entities.GarbageType
+
+class GarbageCleanerUseCases(
+    private val deleteForm: DeleteForm
+) {
+
+    fun selectAll(){
+        deleteForm.selectAll()
+    }
+    fun switchSelection(garbageType: GarbageType){
+        deleteForm.switchSelection(garbageType)
+    }
+    fun startDelete(){
+
+    }
+
 }
--- garbage_clean/domain/src/test/kotlin/GarbageCleanerUseCasesTest.kt
+++ garbage_clean/domain/src/test/kotlin/GarbageCleanerUseCasesTest.kt
@@ -1,10 +1,33 @@
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import yin_kio.garbage_clean.domain.GarbageCleanerUseCases
+import yin_kio.garbage_clean.domain.entities.DeleteForm
+import yin_kio.garbage_clean.domain.entities.GarbageType
 
 class GarbageCleanerUseCasesTest {
 
-    @Test
-    fun testGarbageCleanerInit(){ GarbageCleanerUseCases() }
+    private val deleteForm: DeleteForm = mockk()
+    private val useCases = GarbageCleanerUseCases(deleteForm)
 
+
+    @Test
+    fun testSelectAll(){
+        coEvery { deleteForm.selectAll() } returns Unit
+
+        useCases.selectAll()
+
+        coVerify { deleteForm.selectAll() }
+    }
+
+    @Test
+    fun testSwitchSelection(){
+        coEvery { deleteForm.switchSelection(GarbageType.Apk) } returns Unit
+
+        useCases.switchSelection(GarbageType.Apk)
+
+        coVerify { deleteForm.switchSelection(GarbageType.Apk) }
+    }
 
 }

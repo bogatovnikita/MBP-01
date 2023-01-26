afc4f9aa458608bba9e355fae324ac4d6efd44f4
yin_kio <jamycake@hotmail.com>
2023-01-26 15:04:11 +0300

    garbage_clean: add data module

---
+++ garbage_clean/data/src/main/java/yin_kio/garbage_clean/data/FilesImpl.kt
@@ -0,0 +1,19 @@
+package yin_kio.garbage_clean.data
+
+import android.os.Environment
+import yin_kio.file_utils.FileUtilsImpl
+import yin_kio.garbage_clean.domain.gateways.Files
+import java.io.File
+
+class FilesImpl : Files {
+
+    private val fileUtils = FileUtilsImpl()
+
+    override suspend fun delete(paths: List<String>) {
+        fileUtils.deleteFiles(paths.map { File(it) })
+    }
+
+    override suspend fun getAll(): List<String> {
+        return fileUtils.getAllFiles(Environment.getExternalStorageDirectory()).map { it.absolutePath }
+    }
+}

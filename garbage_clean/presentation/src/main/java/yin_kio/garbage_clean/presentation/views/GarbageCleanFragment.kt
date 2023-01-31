63565a34312323dc47746446fff2c3a6c72e9b3c
yin_kio <jamycake@hotmail.com>
2023-01-31 16:23:01 +0300

    garbage_clean.presentation: add garbage showing

--- garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/views/GarbageCleanFragment.kt
+++ garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/views/GarbageCleanFragment.kt
@@ -2,13 +2,18 @@ package yin_kio.garbage_clean.presentation.views
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.recycler_adapter.recyclerAdapter
 import jamycake.lifecycle_aware.lifecycleAware
 import yin_kio.garbage_clean.presentation.R
 import yin_kio.garbage_clean.presentation.databinding.FragmentGarbageCleanBinding
+import yin_kio.garbage_clean.presentation.databinding.ListItemGarbageBinding
+import yin_kio.garbage_clean.presentation.models.ScreenState
+import yin_kio.garbage_clean.presentation.models.UiDeleteFromItem
 import yin_kio.garbage_clean.presentation.view_model.ObservableScreenViewModel
 import yin_kio.garbage_clean.presentation.view_model.ScreenViewModelFactory
 
@@ -17,18 +22,24 @@ class GarbageCleanFragment : Fragment(R.layout.fragment_garbage_clean) {
     private val binding: FragmentGarbageCleanBinding by viewBinding()
     private val viewModel: ObservableScreenViewModel by lifecycleAware { screenViewModel() }
 
-
-
+    private val adapter by lazy { adapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            viewModel.state.collect{
+        binding.recycler.adapter = adapter
+        setupObserver()
+    }
 
+    private fun setupObserver() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.state.collect {
+                updateUi(it)
             }
         }
     }
 
-
+    private fun updateUi(it: ScreenState) {
+        adapter.submitList(it.deleteFormItems)
+    }
 
 
 
@@ -37,5 +48,16 @@ class GarbageCleanFragment : Fragment(R.layout.fragment_garbage_clean) {
         applicationContext = requireActivity().applicationContext,
         androidViewModel = this
     )
+
+
+    private fun adapter() = recyclerAdapter<UiDeleteFromItem, ListItemGarbageBinding>(
+        onBind = { item, holder ->
+            icon.setImageDrawable(ContextCompat.getDrawable(requireContext(), item.iconRes))
+            name.text = item.name
+            size.text = item.size
+            checkbox.isChecked
+        }
+    )
+
 
 }

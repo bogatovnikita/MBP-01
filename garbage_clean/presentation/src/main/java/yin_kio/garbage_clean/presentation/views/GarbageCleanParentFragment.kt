d245f628037617e424d03428549485efab489d17
yin_kio <jamycake@hotmail.com>
2023-02-01 10:52:53 +0300

    garbage_clean.presentation: add ads showing

--- garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/views/GarbageCleanParentFragment.kt
+++ garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/views/GarbageCleanParentFragment.kt
@@ -2,11 +2,13 @@ package yin_kio.garbage_clean.presentation.views
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.ads.showInter
 import jamycake.lifecycle_aware.lifecycleAware
 import yin_kio.garbage_clean.domain.out.DeleteProgressState
 import yin_kio.garbage_clean.presentation.R
@@ -26,13 +28,25 @@ class GarbageCleanParentFragment : Fragment(R.layout.fragment_parent) {
             viewModel.state.collect{
                 showPermissionScreen(it.hasPermission)
                 showDeleteProgress(it)
+                showInter(it)
             }
         }
 
     }
 
+    private fun showInter(screenState: ScreenState){
+        if (screenState.deleteProgressState == DeleteProgressState.Complete
+            && destinationIs(R.id.deleteProgressDialog)
+        ){
+            navController.navigateUp()
+            showInter { Toast.makeText(requireContext(), "NotImplemented yet", Toast.LENGTH_SHORT).show() }
+        }
+    }
+
     private fun showDeleteProgress(it: ScreenState) {
-        if (it.deleteProgressState == DeleteProgressState.Progress) {
+        if (it.deleteProgressState == DeleteProgressState.Progress
+            && destinationIs(R.id.garbageCleanFragment)
+        ) {
             navController.navigate(R.id.deleteProgressDialog)
         }
     }

c44941f1ad53827cd6a492f70b66492ac06ccb98
yin_kio <jamycake@hotmail.com>
2023-02-02 11:47:30 +0300

    garbage_clean.presentation: pass no deletable files impl to factory

--- garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/view_model/ScreenViewModelFactory.kt
+++ garbage_clean/presentation/src/main/java/yin_kio/garbage_clean/presentation/view_model/ScreenViewModelFactory.kt
@@ -3,10 +3,7 @@ package yin_kio.garbage_clean.presentation.view_model
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import yin_kio.garbage_clean.data.FileSystemInfoProviderImpl
-import yin_kio.garbage_clean.data.FilesImpl
-import yin_kio.garbage_clean.data.OlejaAds
-import yin_kio.garbage_clean.data.PermissionsImpl
+import yin_kio.garbage_clean.data.*
 import yin_kio.garbage_clean.domain.GarbageCleanFactory
 import yin_kio.garbage_clean.presentation.presenter.ScreenPresenter
 
@@ -23,7 +20,8 @@ class ScreenViewModelFactory  {
                 coroutineScope = viewModelScope,
                 fileSystemInfoProvider = FileSystemInfoProviderImpl(applicationContext),
                 permissions = PermissionsImpl(applicationContext),
-                ads = OlejaAds(applicationContext)
+                ads = OlejaAds(applicationContext),
+                noDeletableFiles = NoDeletableFilesImpl(applicationContext)
             )
             val viewModel = ScreenViewModel(useCases)
 

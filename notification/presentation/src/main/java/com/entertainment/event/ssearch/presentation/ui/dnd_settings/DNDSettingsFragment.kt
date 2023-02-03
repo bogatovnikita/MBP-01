05a9dd4b602e4efae524cfde600b841621e48e17
dimap <dima.pasyutin2@gmail.com>
2023-02-03 15:04:13 +0300

    notification:presentation: add dnd fragment

---
+++ notification/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/dnd_settings/DNDSettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.entertainment.event.ssearch.presentation.ui.dnd_settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.entertainment.event.ssearch.presentation.R
+import com.entertainment.event.ssearch.presentation.databinding.FragmentDNDSettingsBinding
+
+class DNDSettingsFragment : Fragment(R.layout.fragment_d_n_d_settings) {
+
+    private val binding: FragmentDNDSettingsBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        with(binding) {
+            dayPicker.setOnChosenDaysListener {  }
+        }
+    }
+
+    private fun initNumberPicker() {
+        with(binding) {
+        }
+    }
+
+}

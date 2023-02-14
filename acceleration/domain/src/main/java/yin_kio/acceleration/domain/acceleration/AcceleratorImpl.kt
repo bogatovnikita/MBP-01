e5006f6ad120aea94d58d4b9de779c90b6555077
YinKio <jamycake@hotmail.com>
2023-02-14 14:43:33 +0300

    acceleration.domain.acceleration: add apps stopping to accelerate

--- acceleration/domain/src/main/java/yin_kio/acceleration/domain/acceleration/AcceleratorImpl.kt
+++ acceleration/domain/src/main/java/yin_kio/acceleration/domain/acceleration/AcceleratorImpl.kt
@@ -1,13 +1,18 @@
 package yin_kio.acceleration.domain.acceleration
 
+import yin_kio.acceleration.domain.bg_uploading.AppsForm
+import yin_kio.acceleration.domain.gateways.Apps
+
 class AcceleratorImpl(
-    private val accelerationOuter: AccelerationOuter
+    private val accelerationOuter: AccelerationOuter,
+    private val appsForm: AppsForm,
+    private val apps: Apps
 ) : Accelerator {
 
     override fun accelerate(){
         accelerationOuter.showAccelerateProgress()
+        apps.stop(appsForm.selectedApps)
         accelerationOuter.showInter()
-        accelerationOuter.complete()
     }
 
 }
--- acceleration/domain/src/test/kotlin/AcceleratorTest.kt
+++ acceleration/domain/src/test/kotlin/AcceleratorTest.kt
@@ -1,22 +1,35 @@
+import io.mockk.coEvery
 import io.mockk.coVerifySequence
+import io.mockk.mockk
 import io.mockk.spyk
 import org.junit.jupiter.api.Test
 import yin_kio.acceleration.domain.acceleration.AccelerationOuter
 import yin_kio.acceleration.domain.acceleration.AcceleratorImpl
+import yin_kio.acceleration.domain.bg_uploading.AppsForm
+import yin_kio.acceleration.domain.gateways.Apps
 
 class AcceleratorTest {
 
     private val accelerationOuter: AccelerationOuter = spyk()
-    private val runner = AcceleratorImpl(accelerationOuter)
+    private val apps: Apps = spyk()
+    private val appsForm: AppsForm = mockk()
+    private val runner = AcceleratorImpl(
+        accelerationOuter = accelerationOuter,
+        apps = apps,
+        appsForm = appsForm
+    )
 
     @Test
-    fun testRun(){
+    fun testAccelerate(){
+        val expectedOut = listOf("app1", "app2")
+        coEvery { appsForm.selectedApps } returns expectedOut
+
         runner.accelerate()
 
         coVerifySequence {
             accelerationOuter.showAccelerateProgress()
+            apps.stop(expectedOut)
             accelerationOuter.showInter()
-            accelerationOuter.complete()
         }
     }
 

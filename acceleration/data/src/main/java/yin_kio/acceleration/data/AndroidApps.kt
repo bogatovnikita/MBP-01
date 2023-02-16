3be42098a97b0ec326ac37feaa6dcc63e6b47e67
YinKio <jamycake@hotmail.com>
2023-02-16 11:52:12 +0300

    acceleration.data: add apps stopping and exclude retrieving of this app

--- acceleration/data/src/main/java/yin_kio/acceleration/data/AndroidApps.kt
+++ acceleration/data/src/main/java/yin_kio/acceleration/data/AndroidApps.kt
@@ -1,9 +1,11 @@
 package yin_kio.acceleration.data
 
+import android.app.ActivityManager
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import yin_kio.acceleration.domain.gateways.Apps
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class AndroidApps(
     private val context: Context
@@ -15,9 +17,19 @@ class AndroidApps(
         val currentTime = Calendar.getInstance().timeInMillis
         val stats = usageStatsManager.queryUsageStats(
             UsageStatsManager.INTERVAL_DAILY,
-            currentTime - 60 * 1000,
+            currentTime - TimeUnit.MINUTES.toMillis(1),
             currentTime
         )
         return stats.map { it.packageName }
+            .filter { it != context.packageName } // Лучше бы это перенести в домен
+    }
+
+    override fun stop(apps: Collection<String>) {
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        apps.forEach { packageName ->
+            runCatching {
+                activityManager.killBackgroundProcesses(packageName)
+            }
+        }
     }
 }

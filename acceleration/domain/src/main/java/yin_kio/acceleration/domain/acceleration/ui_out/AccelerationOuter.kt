6fae50571265b58c4e5cae2480fba832f9044b13
YinKio <jamycake@hotmail.com>
2023-02-16 17:27:15 +0300

    acceleration.domain: refactor extract AccelerationNavigator.kt

--- acceleration/domain/src/main/java/yin_kio/acceleration/domain/acceleration/ui_out/AccelerationOuter.kt
+++ acceleration/domain/src/main/java/yin_kio/acceleration/domain/acceleration/ui_out/AccelerationOuter.kt
@@ -1,22 +1,11 @@
 package yin_kio.acceleration.domain.acceleration.ui_out
 
-interface AccelerationOuter {
-
-    fun close()
-
-    fun showAccelerateProgress()
-    fun showInter()
-
-    fun showPermission()
-    fun showSelectableAcceleration()
+interface AccelerationOuter : AccelerationNavigator {
 
     fun showRamInfo(ramInfo: RamInfoOut)
 
     fun showAppsState(appsState: AppsState)
 
     fun givePermission()
-
-
-    fun complete()
 
 }

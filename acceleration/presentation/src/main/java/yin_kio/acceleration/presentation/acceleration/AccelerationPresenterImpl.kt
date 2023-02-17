7d99035865f84e0282835c05fe232acd7089c2ec
YinKio <jamycake@hotmail.com>
2023-02-17 17:26:34 +0300

    acceleration.presentation: impl AccelerationPresenter.presentRamInfoOut(...)

---
+++ acceleration/presentation/src/main/java/yin_kio/acceleration/presentation/acceleration/AccelerationPresenterImpl.kt
@@ -0,0 +1,24 @@
+package yin_kio.acceleration.presentation.acceleration
+
+import android.content.Context
+import android.text.format.Formatter.formatFileSize
+import yin_kio.acceleration.domain.acceleration.ui_out.RamInfoOut
+
+class AccelerationPresenterImpl(
+    private val applicationContext: Context
+) : AccelerationPresenter {
+
+    override fun presentRamInfoOut(ramInfoOut: RamInfoOut): RamInfo {
+
+        return RamInfo(
+            progress = (ramInfoOut.occupied.toDouble() / ramInfoOut.total).toFloat(),
+            occupied = formatSize(ramInfoOut.occupied),
+            available = formatSize(ramInfoOut.available),
+            total = formatSize(ramInfoOut.total)
+        )
+    }
+
+    private fun formatSize(size: Long) : String{
+        return formatFileSize(applicationContext, size)
+    }
+}

3a299bfe055d7fd30093afce9f0dd8784b25dae8
YinKio <jamycake@hotmail.com>
2023-02-20 15:22:31 +0300

    acceleration.presentation: impl acceleration dialog

--- acceleration/presentation/src/main/java/yin_kio/acceleration/presentation/acceleration/AccelerationDialog.kt
+++ acceleration/presentation/src/main/java/yin_kio/acceleration/presentation/acceleration/AccelerationDialog.kt
@@ -1,7 +1,42 @@
 package yin_kio.acceleration.presentation.acceleration
 
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import yin_kio.acceleration.presentation.R
 
 class AccelerationDialog : DialogFragment(R.layout.dialog_accelerate) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupLayoutParams()
+    }
+
+    private fun setupLayoutParams() {
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return noCancelableDialog()
+    }
+
+    private fun noCancelableDialog() = object : Dialog(requireContext()) {
+        init {
+            setCancelable(false)
+            setCanceledOnTouchOutside(false)
+
+            window?.setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onBackPressed() {}
+    }
+
+
 }

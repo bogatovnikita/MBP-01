83a7c182cc9172cc65f6e5ace26f550b55009159
dimap <dima.pasyutin2@gmail.com>
2023-02-22 05:32:47 +0300

    device_info:presentation: complete first step adding expandable recycler view

---
+++ device_info/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/device_info/DeviceInfoFragment.kt
@@ -0,0 +1,12 @@
+package com.entertainment.event.ssearch.presentation.ui.device_info
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.entertainment.event.ssearch.presentation.R
+import com.entertainment.event.ssearch.presentation.databinding.FragmentDeviceInfoBinding
+
+class DeviceInfoFragment : Fragment(R.layout.fragment_device_info) {
+
+    private val binding: FragmentDeviceInfoBinding by viewBinding()
+
+}
---
+++ device_info/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/device_info/DeviceInfoViewModel.kt
@@ -0,0 +1,6 @@
+package com.entertainment.event.ssearch.presentation.ui.device_info
+
+import androidx.lifecycle.ViewModel
+
+class DeviceInfoViewModel : ViewModel() {
+}

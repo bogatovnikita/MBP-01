2b8afb654e3c3870b5564f3556c8877243b30868
dimap <dima.pasyutin2@gmail.com>
2023-02-24 02:44:37 +0300

    device_info:presentation and test: backup saving

--- device_info/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/device_info/DeviceInfoFragment.kt
+++ device_info/presentation/src/main/java/com/entertainment/event/ssearch/presentation/ui/device_info/DeviceInfoFragment.kt
@@ -1,5 +1,7 @@
 package com.entertainment.event.ssearch.presentation.ui.device_info
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.entertainment.event.ssearch.presentation.R
@@ -9,4 +11,8 @@ class DeviceInfoFragment : Fragment(R.layout.fragment_device_info) {
 
     private val binding: FragmentDeviceInfoBinding by viewBinding()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding
+    }
 }

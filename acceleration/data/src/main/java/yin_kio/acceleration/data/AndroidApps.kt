f01fa2b29bf3e826fce35324a3a38b26fbc90102
YinKio <jamycake@hotmail.com>
2023-02-24 14:20:45 +0300

    acceleration.data: impl AndroidApps.kt with new interface

--- acceleration/data/src/main/java/yin_kio/acceleration/data/AndroidApps.kt
+++ acceleration/data/src/main/java/yin_kio/acceleration/data/AndroidApps.kt
@@ -4,23 +4,29 @@ import android.app.ActivityManager
 import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.os.Build
 import yin_kio.acceleration.domain.gateways.Apps
+import yin_kio.acceleration.domain.selectable_acceleration.entities.App
 import java.util.concurrent.TimeUnit
 
 class AndroidApps(
     private val context: Context
 ) : Apps{
 
-    override suspend fun provide(): List<String> {
-        val stats = getStats()
-
-        return stats.map { it.packageName }
+    override suspend fun provide(): List<App> {
+        return getStats()
+            .map { getApp(it) }
             .filter {
-                it != context.packageName
-                && context.packageManager.getLaunchIntentForPackage(it) != null
-                && !it.contains(".test")
+                it.packageName.isNotEmpty()
+                && it.packageName != context.packageName
+                && context.packageManager.getLaunchIntentForPackage(it.packageName) != null
+                && !it.packageName.contains(".test")
             }
     }
+
 
     private fun getStats(): List<UsageStats> {
         val usageStatsManager =
@@ -33,11 +39,55 @@ class AndroidApps(
         ) ?: listOf()
     }
 
-    override suspend fun stop(apps: Collection<String>) {
+
+
+    private fun getApp(it: UsageStats) : App{
+        return try {
+            App(
+                packageName = it.packageName,
+                name = getName(it.packageName),
+                icon = getIcon(it.packageName)
+            )
+        } catch (ex: Exception){
+            App() // emptyApp
+        }
+    }
+
+    private fun getName(packageName: String): String{
+        return getPackageInfo(packageName).applicationInfo.loadLabel(context.packageManager).toString()
+    }
+
+
+    private fun getIcon(packageName: String) : Drawable{
+        return getPackageInfo(packageName).applicationInfo.loadIcon(context.packageManager)
+    }
+
+
+    private fun getPackageInfo(packageName: String) : PackageInfo {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0))
+        } else {
+            @Suppress("DEPRECATION") context.packageManager.getPackageInfo(packageName, 0)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    override suspend fun stop(apps: Collection<App>) {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        apps.forEach { packageName ->
+        apps.forEach {
             runCatching {
-                activityManager.killBackgroundProcesses(packageName)
+                activityManager.killBackgroundProcesses(it.packageName)
             }
         }
     }

f56378a08b2b85d46c72521613562501843bbf6e
YinKio <jamycake@hotmail.com>
2023-03-02 11:44:31 +0300

    applications.domain: refactor

--- applications/domain/src/main/java/yin_kio/applications/domain/core/EstablishedAppsForm.kt
+++ applications/domain/src/main/java/yin_kio/applications/domain/core/EstablishedAppsForm.kt
@@ -2,14 +2,14 @@ package yin_kio.applications.domain.core
 
 interface EstablishedAppsForm {
 
-    fun isAppSelected(app: App) : Boolean
     var content: List<App>
-    fun sort()
-
     var isVisible: Boolean
     val isAllSelected: Boolean
     val selectedApps: Collection<App>
 
+
+    fun isAppSelected(app: App) : Boolean
+    fun sort()
     fun switchIsAllSelected()
 
 }
